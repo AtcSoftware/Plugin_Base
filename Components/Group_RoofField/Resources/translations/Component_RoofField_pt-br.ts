aeb2483ac47146f0a32f4cf74630099c0e1e37e2
Rodrigo G. Bertoni <rgbertoni@gmail.com>
2020-12-24 15:16:33 -0300

    0.1_windows

---
+++ Components/Group_RoofField/Resources/translations/Component_RoofField_pt-br.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR" sourcelanguage="en">
+<context>
+    <name>Component_RoofField</name>
+    <message>
+        <location filename="RoofField.py" line="31"/>
+        <source>Roof field</source>
+        <translation>Teste Água(Pano, Vertente) de cobertura</translation>
+    </message>
+    <message>
+        <location filename="RoofField.py" line="32"/>
+        <source>Create a roof field</source>
+        <translation>Criar água(pano, vertente) de cobertura</translation>
+    </message>
+</context>
+</TS>
